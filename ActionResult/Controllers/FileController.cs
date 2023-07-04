c5e226cedfddc0af77ffe2421c7153e8ab224c7f
Leyla Shafiyeva <llshafi03@gmail.com>
2023-07-04 23:45:14 +0400

    Add project files.

---
+++ ActionResult/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionResult.Controllers
+{
+    //FileResult
+    public class FileController : Controller
+    {
+        [Route("File")]
+        public VirtualFileResult Index()
+        {
+            return File("/sample.pdf","application/pdf");
+        }
+    }
+}
---
+++ ActionResult/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using ActionResult.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        ////// Content Result
+        //public IActionResult Index()
+        //{
+        //    return Content("<h1>Hello, I am Leyla</h1>", "text/html");
+        //}
+
+        ////// Json Result
+        //public IActionResult Index()
+        //{
+        //    Human human = new Human()
+        //    {
+        //        Id= 1,
+        //        Name = "Leyla",
+        //        Surname = "Shafiyeva",
+        //        Age = 19
+        //    };
+        //    return Json(human);
+        //}
+
+        ////// RedirectResult
+        //public IActionResult Index()
+        //{
+        //    return Redirect("https://www.google.com/");
+        //}
+
+        ////// EmptyResult
+        //public IActionResult Index()
+        //{
+        //    return new EmptyResult();
+        //}
+
+        //ViewResult
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
